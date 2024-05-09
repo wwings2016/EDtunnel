cf588d20470442cbb4814a6d3b12c782df149568
wwings2016 <102454098+wwings2016@users.noreply.github.com>
2024-05-09 17:54:35 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '524ab1f0afa4-d342d11e-d424-4583-b36e';
+let userID = '0b171b9f-97ad-4b54-8498-8fe7e49876a7';
 
 const พร็อกซีไอพีs = ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'workers.cloudflare.cyou'];
 
